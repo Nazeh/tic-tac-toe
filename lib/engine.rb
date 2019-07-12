9e26a830f5918f293bc4de330a1b1f0b0e556650
Nazeh <ar.nazeh@gmail.com>
2019-07-12 19:52:46 +0300

    start engine done

--- lib/engine.rb
+++ lib/engine.rb
@@ -3,8 +3,20 @@
 # Engine creates messeges to communicate with users and gets their inputs as well as running the game logic
 module Engine
   def self.start
-    # code
+    @hash = {}
+
+    while @hash.empty?
+      puts 'choose X or O'
+      user_input = gets.chomp.to_s.upcase
+      break if %w[X O].include?(user_input)
+
+      print 'please try again '
+    end
+
+    @hash[:p1] = user_input
+    @hash[:p2] = @hash[:p1] == 'X' ? 'O' : 'X'
   end
+
   def self.play
     # code
   end
@@ -12,4 +24,6 @@ end
 
 # test
 
+Engine.start
 puts Engine.play
+puts 'thanks for playing'
