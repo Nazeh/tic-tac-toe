a7ae4e366d4da0f3f299ab67a0426aa3137783f3
Nazeh <ar.nazeh@gmail.com>
2019-07-12 16:45:58 +0300

    add private methods row_inedx, col_index and ..

--- lib/board.rb
+++ lib/board.rb
@@ -3,7 +3,7 @@
 # Board class initiate a 2D Array, and it can update it, fetch it
 # and return the row, col and/or diagonals of any cell
 class Board
-  att_reader :board
+  attr_reader :board
 
   def initialize
     @board = [[1, 2, 3],
@@ -12,15 +12,37 @@ class Board
   end
 
   def update(cell, sign)
-    row = (cell - 1) / 3
-    col = (cell - 1) % 3
-    @board[row][col] = sign
+    @board[row_index(cell)][col_index(cell)] = sign
+  end
+
+  def get_row_col_diagonals(cell)
+    rcd = [get_row(cell), get_col(cell)]
+  end
+
+  private
+
+  def row_index(cell)
+    (cell - 1) / 3
+  end
+
+  def col_index(cell)
+    (cell - 1) % 3
+  end
+
+  def get_row(cell)
+    board[row_index(cell)]
+  end
+
+  def get_col(cell)
+    col = []
+    @board.each { |row| col << row[col_index(cell)] }
+    col
   end
 end
 
-# testing
+# test board.rb
 board = Board.new
 
 board.update(4, 'X')
 
-puts board.board.to_s
+puts board.get_row_col_diagonals(4).to_s
