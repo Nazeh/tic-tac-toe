6af3ea1cef0a472dc6e95b3c79af34a4108745c7
Nazeh <ar.nazeh@gmail.com>
2019-07-12 19:24:09 +0300

    start the engine branch

---
+++ lib/engine.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Engine creates messeges to communicate with users and gets their inputs as well as running the game logic
+module Engine
+  def self.start
+    # code
+  end
+  def self.play
+    # code
+  end
+end
+
+# test
+
+puts Engine.play
