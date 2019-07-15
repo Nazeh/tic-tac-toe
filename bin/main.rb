2dc168524b820901e2a43df1478cbeec9a4a0549
Nazeh <ar.nazeh@gmail.com>
2019-07-15 16:21:20 +0300

    finished engine without UI

--- bin/main.rb
+++ bin/main.rb
@@ -4,21 +4,18 @@ require_relative '../lib/board.rb'
 require_relative '../lib/engine.rb'
 require_relative '../lib/game.rb'
 
-# Initiate a new board for UI
-board = Board.new
-
-# Initiate sudo user interface (Place holder until UI)
-puts board.board[0].to_s
-puts board.board[1].to_s
-puts board.board[2].to_s
-
 # Start the game
 loop do
   game = Game.new
   board = Board.new
+  # Initiate sudo user interface (Place holder until UI)
+  puts board.board[0].to_s
+  puts board.board[1].to_s
+  puts board.board[2].to_s
+
   Engine.play(game, board)
-  puts 'do you want to play again? Y or N'
+  puts "\nWould you like to play again? 'Y' or 'N'"
   break unless gets.chomp.downcase.to_s == 'y'
 end
 # thanks after exsiting (Place holder until UI)
-puts 'Thank you for playing out implementation of Tic Tac Toe'
+puts "\nThank you for playing out implementation of Tic Tac Toe!"
--- lib/engine.rb
+++ lib/engine.rb
@@ -9,36 +9,38 @@ module Engine
     @marked_cells = []
 
     while game_status == 'continue'
+      # place holder until updating UI
+      show_board(board)
+
       # update cell
       cell = prompt_cell
       @marked_cells << cell
 
-      # update board
-      board.update(cell, sign)
-
-      # place holder until updating UI
-      puts board.board[0].to_s
-      puts board.board[1].to_s
-      puts board.board[2].to_s
-
       # update game status and switch player
+      board.update(cell, sign)
       row_col_diagonals = board.get_row_col_diagonals(cell)
       game_status = game.status(row_col_diagonals, sign)
       sign = (@players_signs - [sign]).first
     end
 
-    puts game_status
+    puts "\n", game_status
+  end
+
+  def self.show_board(board)
+    # place holder until UI
+    puts "\n"
+    puts board.board[0].to_s
+    puts board.board[1].to_s
+    puts board.board[2].to_s
   end
 
   def self.initiate_players_signs
     @players_signs = []
 
     while @players_signs.empty?
-      puts 'choose X or O'
+      puts "\nPlayer 1, Please choose a mark: 'X' or 'O' ?"
       user_input = gets.chomp.to_s.upcase
       break if %w[X O].include?(user_input)
-
-      print 'please try again '
     end
 
     @players_signs[0] = user_input
@@ -48,11 +50,10 @@ module Engine
   def self.prompt_cell
     cell = nil
     while cell.nil?
-      puts 'Please choose a cell'
+      puts "\nPlayer #{@marked_cells.length % 2 + 1}, Where would you like to put your mark?"
       answer = gets.chomp.to_i
       cell = answer unless @marked_cells.include?(answer) || [1, 2, 3, 4, 5, 6, 7, 8, 9].include?(answer) == false
     end
-    puts ' i returnd'
     cell
   end
 end
